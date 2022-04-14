60cb2dbc0f763003373814a4d8942089e1cfdf77
Kenpachi080 <saxah23332@gmail.com>
2022-04-15 00:43:26 +0600

    first commit

--- database/migrations/2022_04_14_184058_create_langs_table.php
+++ database/migrations/2022_04_14_184058_create_langs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobSkillsTable extends Migration
+class CreateLangsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateJobSkillsTable extends Migration
      */
     public function up()
     {
-        Schema::create('job_skills', function (Blueprint $table) {
+        Schema::create('langs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
@@ -26,6 +26,6 @@ class CreateJobSkillsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('job_skills');
+        Schema::dropIfExists('langs');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticleController as Articles;
-use App\Http\Controllers\PortfolioController as PortfolioController;
-use App\Http\Controllers\QuestionController as QuestionController;
-use App\Http\Controllers\ApiController as ApiController;
 
 
 /*
@@ -18,22 +14,3 @@ use App\Http\Controllers\ApiController as ApiController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
-Route::get('/article', [Articles::class, 'viewArticle']);
-Route::get('/comment', [Articles::class, 'viewComment']);
-Route::get('/portfolio', [PortfolioController::class, 'view']);
-Route::get('/categoryes', [PortfolioController::class, 'categoryes']);
-Route::post('/comment', [Articles::class, 'commentAdd']);
-Route::get('/mainquestion', [QuestionController::class, 'main']);
-Route::get('/childquestion', [QuestionController::class, 'child']);
-
-Route::post('/callback', [ApiController::class, 'callback']);
-
-Route::group(['prefix' => 'vacancy'], function () {
-    Route::get('/type', [ApiController::class, 'typejobs']);
-    Route::get('/simplejob', [ApiController::class, 'simplejob']);
-    Route::post('/job', [ApiController::class, 'job']);
-});
-
-Route::get('/reviews', [ApiController::class, 'reviews']);
