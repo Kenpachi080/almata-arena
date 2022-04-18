bcee8546eede494e799602c83f601bb4ea4b3d29
Sarkhan Ibragimov <88957503+Kenpachi080@users.noreply.github.com>
2022-04-18 22:44:30 +0600

    Add files via upload

---
+++ database/migrations/2022_04_18_054137_create_navigations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavigationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigations', function (Blueprint $table) {
+            $table->id();
+            $table->string('about');
+            $table->string('service');
+            $table->string('objects');
+            $table->string('clients');
+            $table->string('reviews');
+            $table->string("news");
+            $table->string('contacts');
+            $table->string('lang');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigations');
+    }
+}
